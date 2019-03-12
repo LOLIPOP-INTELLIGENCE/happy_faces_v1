64d9d2c254b55bfcbe78a76df446f24deaaa2efb
Manas Bam <manaslegodesigns@gmail.com>
2019-03-12 22:15:35 +0530

    Initial Commit

---
+++ happy_faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  happy_faces
+//
+//  Created by Arati Bam on 12/03/19.
+//  Copyright © 2019 Manas Bam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
