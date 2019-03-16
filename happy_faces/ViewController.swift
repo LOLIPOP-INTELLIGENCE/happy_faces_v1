d7b421ac6080defd916f7ee5646c290516f6fce7
Manas Bam <manaslegodesigns@gmail.com>
2019-03-16 18:00:37 +0530

    committed

--- happy_faces/ViewController.swift
+++ happy_faces/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    let model=happy_faces()
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func takePhoto(_ sender: Any) {
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
+            let imagePicker=UIImagePickerController()
+            imagePicker.delegate=self
+            imagePicker.sourceType=UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing=false
+            self.present(imagePicker,animated: true, completion: nil)
+        }
+        else{
+            print("NO")
+        }
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage=info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            myImage.contentMode = .scaleToFill
+            myImage.image=pickedImage
+            let final_image=image(with: myImage.image!, scaledTo: CGSize(width: 48.0, height: 48.0))
+            let output = try? model.prediction(image:final_image as! CVPixelBuffer)
+            print(output!)
+            
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func image(with image: UIImage, scaledTo newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage ?? UIImage()
+    }
+    
+    
 }
+
 
